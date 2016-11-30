b0d231e5a10070a938874b4eab26384ef2cc47a7
xmyqsh <xmyqsh@gmail.com>
2016-11-30 10:30:54 +0800

    modify pixel mean to ucd

--- lib/fast_rcnn/config.py
+++ lib/fast_rcnn/config.py
@@ -187,7 +187,8 @@ __C.DEDUP_BOXES = 1./16.
 # Pixel mean values (BGR order) as a (1, 1, 3) array
 # We use the same pixel mean for all networks even though it's not exactly what
 # they were trained with
-__C.PIXEL_MEANS = np.array([[[102.9801, 115.9465, 122.7717]]])
+#__C.PIXEL_MEANS = np.array([[[102.9801, 115.9465, 122.7717]]])
+__C.PIXEL_MEANS = np.array([[[101.38770294, 100.59334564, 97.58388519]]])
 
 # For reproducibility
 __C.RNG_SEED = 3
